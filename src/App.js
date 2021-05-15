67bb71eb832a3251a69f686bd140ad8f140cee35
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-05-15 17:34:04 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
+import Nav from './components/Nav';
 
 function App() {
   return (
+  <div className="outerWrap">
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
     </div>
+    <div className="musicControls">
+      Music Controls
+    </div>
+  </div>
   );
 }
 
