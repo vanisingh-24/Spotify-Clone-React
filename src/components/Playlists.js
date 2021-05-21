a7937357cb45b75820b5ca9a445b9f0b331c040b
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-05-21 14:41:16 +0530

    bugs fixed in playlist component

--- src/components/Playlists.js
+++ src/components/Playlists.js
@@ -1,76 +1,171 @@
-import React from 'react';
-import {ReactComponent as PlayIcon} from '../svgs/play.svg';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { ReactComponent as PlayIcon } from "../svgs/play.svg";
+import { Link } from "react-router-dom";
 
 const Playlists = (props) => {
-    const dataPlaylists = [
-        {
-            id: 101,
-            category_id: 3,
-            name: "Home Playlist 1",
-            img: 'https://images.unsplash.com/photo-1587201572498-2bc131fbf113?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=924&q=80',
-            desc: 'Lorem ipsum'
-        },
-        {
-            id: 102,
-            category_id: 3,
-            name: "Home Playlist 2",
-            img: 'https://images.unsplash.com/photo-1587151711096-23c51f92c920?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80',
-            desc: 'Lorem ipsum'
-        },
-        {
-            id: 103,
-            category_id: 3,
-            name: "Home Playlist 3",
-            img: 'https://images.unsplash.com/photo-1587223075055-82e9a937ddff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80',
-            desc: 'Lorem ipsum'
-        },
-        {
-            id: 104,
-            category_id: 1,
-            name: "Focus Playlist 1",
-            img: 'https://images.unsplash.com/photo-1587165282385-fe9bbf5eb1a0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80',
-            desc: 'Lorem ipsum'
-        },
-        {
-            id: 106,
-            category_id: 2,
-            name: "Mood Playlist 1",
-            img: 'https://images.unsplash.com/photo-1587220111918-7a5c0f0c46f5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80',
-            desc: 'Lorem ipsum'
-        },
-        {
-            id: 107,
-            category_id: 2,
-            name: "Mood Playlist 2",
-            img: 'https://images.unsplash.com/photo-1587220111918-7a5c0f0c46f5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80',
-            desc: 'Lorem ipsum'
-        }
-    ]
+  const dataPlaylists = [
+    {
+      id: 101,
+      category_id: 3,
+      name: "Home Playlist 1",
+      img:
+        "https://images.unsplash.com/photo-1587201572498-2bc131fbf113?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=924&q=80",
+      desc: "Lorem ipsum",
+    },
+    {
+      id: 102,
+      category_id: 3,
+      name: "Home Playlist 2",
+      img:
+        "https://images.unsplash.com/photo-1587151711096-23c51f92c920?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80",
+      desc: "Lorem ipsum",
+    },
+    {
+      id: 103,
+      category_id: 3,
+      name: "Home Playlist 3",
+      img:
+        "https://images.unsplash.com/photo-1587223075055-82e9a937ddff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80",
+      desc: "Lorem ipsum",
+    },
+    {
+      id: 103,
+      category_id: 3,
+      name: "Home Playlist 3",
+      img:
+        "https://images.unsplash.com/photo-1587223075055-82e9a937ddff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80",
+      desc: "Lorem ipsum",
+    },
+    {
+      id: 103,
+      category_id: 3,
+      name: "Home Playlist 3",
+      img:
+        "https://images.unsplash.com/photo-1587223075055-82e9a937ddff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80",
+      desc: "Lorem ipsum",
+    },
+    {
+      id: 103,
+      category_id: 3,
+      name: "Home Playlist 3",
+      img:
+        "https://images.unsplash.com/photo-1587223075055-82e9a937ddff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80",
+      desc: "Lorem ipsum",
+    },
+    {
+      id: 103,
+      category_id: 3,
+      name: "Home Playlist 3",
+      img:
+        "https://images.unsplash.com/photo-1587223075055-82e9a937ddff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80",
+      desc: "Lorem ipsum",
+    },
+    {
+      id: 103,
+      category_id: 3,
+      name: "Home Playlist 3",
+      img:
+        "https://images.unsplash.com/photo-1587223075055-82e9a937ddff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80",
+      desc: "Lorem ipsum",
+    },
+    {
+      id: 103,
+      category_id: 3,
+      name: "Home Playlist 3",
+      img:
+        "https://images.unsplash.com/photo-1587223075055-82e9a937ddff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80",
+      desc: "Lorem ipsum",
+    },
+    {
+      id: 103,
+      category_id: 3,
+      name: "Home Playlist 3",
+      img:
+        "https://images.unsplash.com/photo-1587223075055-82e9a937ddff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80",
+      desc: "Lorem ipsum",
+    },
+    {
+      id: 103,
+      category_id: 3,
+      name: "Home Playlist 3",
+      img:
+        "https://images.unsplash.com/photo-1587223075055-82e9a937ddff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80",
+      desc: "Lorem ipsum",
+    },
+    {
+      id: 103,
+      category_id: 3,
+      name: "Home Playlist 3",
+      img:
+        "https://images.unsplash.com/photo-1587223075055-82e9a937ddff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80",
+      desc: "Lorem ipsum",
+    },
+    {
+      id: 103,
+      category_id: 3,
+      name: "Home Playlist 3",
+      img:
+        "https://images.unsplash.com/photo-1587223075055-82e9a937ddff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80",
+      desc: "Lorem ipsum",
+    },
+    {
+      id: 104,
+      category_id: 1,
+      name: "Focus Playlist 1",
+      img:
+        "https://images.unsplash.com/photo-1587165282385-fe9bbf5eb1a0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
+      desc: "Lorem ipsum",
+    },
+    {
+      id: 105,
+      category_id: 4,
+      name: "Sunday Playlist ",
+      img:
+        "https://images.unsplash.com/photo-1587143602695-c980e283be9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2702&q=80",
+      desc: "Lorem ipsum",
+    },
+    {
+      id: 106,
+      category_id: 2,
+      name: "Mood Playlist 1",
+      img:
+        "https://images.unsplash.com/photo-1587220111918-7a5c0f0c46f5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80",
+      desc: "Lorem ipsum",
+    },
+    {
+      id: 107,
+      category_id: 2,
+      name: "Mood Playlist 2",
+      img:
+        "https://images.unsplash.com/photo-1587220111918-7a5c0f0c46f5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80",
+      desc: "Lorem ipsum",
+    },
+  ];
 
-    const matchedPlaylists = dataPlaylists.filter(playlist => playlist.category_id === props.category_id)
+  let matchedPlaylists = dataPlaylists
+    .filter((playlist) => playlist.category_id === props.category_id)
+    .slice(0, props.limiter);
 
-    return (
-           <div className="cardsWrapInner">
-
-                 {matchedPlaylists.map((playlist,id) => (
-                   <Link to={`/playlist/` + playlist.id}>
-                     <div className="card" key={id}>
-                     <div className="cardImage">
-                       <img src="{playlist.img}" alt="Pic 1" />
-                     </div>
-                     <div className="cardContent">
-                       <h3>{playlist.name}</h3>
-                       <span>{playlists.desc}</span>
-                     </div>
-                     <span className="playIcon">
-                       <PlayIcon />
-                     </span>
-                   </div>
-                   </Link>
-                 ))}
-               </div>
-    )
-}
+  return (
+    <div className="cardsWrapInner">
+      {matchedPlaylists.map((playlist, id) => (
+        <Link to={`/playlist/` + playlist.id} key={id}>
+          <div className="card" key={id}>
+            <div className="cardImage">
+              <img src={playlist.img} alt="Pic 1" />
+            </div>
+            <div className="cardContent">
+              <h3>{playlist.name}</h3>
+              <span>{playlist.desc}</span>
+            </div>
+            <span className="playIcon">
+              <PlayIcon />
+            </span>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
 
 export default Playlists;
